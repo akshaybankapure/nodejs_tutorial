451f642f190446e6e6fd1ebd33308687fee74447
Akshay Bankapure <hotakshay15@gmail.com>
2021-10-14 13:54:57 +0100

    Update 001.js

--- 001.js
+++ 001.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+//I am including a few basic code axamples in this one file iteslt
+//Program 1.
+console.log('Hello World'); //console.log('') is used to print something on the console/terminal. just like printf in C
+
+//Pogram 2
+//This program is to tell the number is bigger or smaller
+constant A=23; //we are taking a stating number for input
+constant B= 43;
+if(A>B){
+  console.log('A is bigger');
+  
+}
+else {
+  console.log('B is larger');
+}
+
+//Program 3
+//
